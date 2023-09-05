cda57bd9b0d2bb66ac7e033c505bc7efd695ddcb
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-09-05 12:23:13 -0300

    feat: get todo by id and create todo

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from "express";
-import { getTodos } from "./logic";
+import { createTodo, getTodoById, getTodos } from "./logic";
 
 const app = express();
 
 app.use(express.json());
 
 app.get("/", getTodos);
+
+app.get("/:todoId", getTodoById);
+
+app.post("/", createTodo);
 
 const PORT = 3000;
 
--- src/logic.ts
+++ src/logic.ts
@@ -1,6 +1,24 @@
 import { Request, Response } from "express";
 import { todoDatabase } from "./database";
+import { v4 as uuidv4 } from 'uuid';
 
 export const getTodos = (req: Request, res: Response) => {
     return res.status(200).json(todoDatabase);
+}
+
+export const getTodoById = (req: Request, res: Response) => {
+    const foundTodo = todoDatabase.find(todo => todo.id === req.params.todoId);
+
+    if(!foundTodo){
+        return res.status(404).json({ message: "Not found any todo with this id"});
+    }
+
+    return res.status(200).json(foundTodo);
+}
+
+export const createTodo = (req: Request, res: Response) => {
+    const newTodo = { id: uuidv4(), title: req.body.title, content: req.body.content };
+    todoDatabase.push(newTodo);
+
+    return res.status(201).json({ message: "Todo sucessfuly created.", todo: newTodo});
 }
